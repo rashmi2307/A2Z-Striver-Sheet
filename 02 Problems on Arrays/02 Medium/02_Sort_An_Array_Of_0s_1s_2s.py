0947171f4ee135d855729f40f0294e5a9f50d541
Rashmi Andhale <rashmiandhale23@gmail.com>
2026-06-23 18:37:20 +0530

    Sort and= array of 0s, 1s, and 2s

---
+++ 02 Problems on Arrays/02 Medium/02_Sort_An_Array_Of_0s_1s_2s.py
@@ -0,0 +1,36 @@
+# Given an array nums consisting of only 0, 1, or 2. Sort the array in non-decreasing order.
+
+# The sorting must be done in-place, without making a copy of the original array.
+
+# Example 1
+# Input: nums = [1, 0, 2, 1, 0]
+# Output: [0, 0, 1, 1, 2]
+# Explanation:
+# The nums array in sorted order has 2 zeroes, 2 ones and 1 two
+
+# Example 2
+# Input: nums = [0, 0, 1, 1, 1]
+# Output: [0, 0, 1, 1, 1]
+# Explanation:
+# The nums array in sorted order has 2 zeroes, 3 ones and zero twos
+
+
+# Brute Force Solution
+# Time Complexity: O(n) where n is the length of the input array
+class Solution:
+    def sortZeroOneTwo(self, nums):
+        n = len(nums)
+        count1, count2, count0 = 0, 0, 0
+        for i in range (n):
+            if nums[i] == 0:
+                count0 += 1
+            if nums[i] == 1:
+                count1 += 1
+            if nums[i] == 2:
+                count2 += 1
+        for i in range (count0):
+            nums[i] = 0
+        for i in range (count0, count0+count1):
+            nums[i] = 1
+        for i in range (count0+count1, n):
+            nums[i] = 2
