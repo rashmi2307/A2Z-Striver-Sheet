02cd881a1ffb80a5e148a7d1133d3900b1545154
Rashmi Andhale <rashmiandhale23@gmail.com>
2026-06-26 22:12:09 +0530

    Set Matrix Zeroes Brute Force

---
+++ 02 Problems on Arrays/02 Medium/10_Set_Matrix_Zeroes.py
@@ -0,0 +1,40 @@
+# Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0. You must do it in place.
+
+# Example 1
+# Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+# Output: [[1,0,1],[0,0,0],[1,0,1]]
+# Explanation:
+# Element at position (1,1) is 0, so set entire row 1 and column 1 to 0.
+
+# Example 2
+# Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+# Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+# Explanation:
+# There are two zeroes: (0,0) and (0,3).
+# Row 0 → all elements become 0
+# Column 0 and column 3 → all elements become 0
+
+
+
+# Brute Force Approach
+# Time Complexity: O(m*n*(m+n)) where m is the number of rows and n is the number of columns in the input matrix
+# Space Complexity: O(1) as we are not using any extra space
+from typing import List
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        m = len(matrix)
+        n = len(matrix[0])
+        for i in range (m):
+            for j in range (n):
+                if matrix[i][j] == 0:
+                    for col in range (n):
+                        if matrix[i][col] != 0:
+                            matrix[i][col] = -1
+                    for row in range(m):
+                        if matrix[row][j] != 0:
+                            matrix[row][j] = -1
+        for i in range (m):
+            for j in range (n):
+                if matrix[i][j] == -1:
+                    matrix[i][j] = 0
+    
